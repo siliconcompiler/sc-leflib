91be3766ae336aa42fdf425874216220fb9da4cd
Peter Gadfort <peter.gadfort@gmail.com>
2024-10-14 11:01:33 -0400

    import installed module

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,6 @@ import os
 
 
 @pytest.fixture
-def scroot():
-    '''Returns an absolute path to the SC root directory.'''
-    mydir = os.path.dirname(__file__)
-    return os.path.abspath(os.path.join(mydir, '..', 'siliconcompiler'))
-
-
-@pytest.fixture
 def datadir(request):
     '''Returns an absolute path to the current test directory's local data
     directory.'''
